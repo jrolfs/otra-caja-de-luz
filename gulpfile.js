c36d47344f958a07cd9a1e9c55caee7ad5172eaf
Jamie Rolfs <jamie.rolfs@gmail.com>
2016-05-25 09:09:12 -0700

    Add gulp plumber to improve ava output

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const gulp = require('gulp');
+const plumber = require('gulp-plumber');
 const ava = require('gulp-ava');
+
 
 gulp.task('test', () => {
   gulp.src('test/**/*.js')
+    .pipe(plumber())
     .pipe(ava());
 });
