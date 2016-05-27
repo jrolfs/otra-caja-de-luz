4a190a3c994a8d72d6d4486cc7f15c4ae8aad107
Jamie Rolfs <jamie.rolfs@gmail.com>
2016-05-26 18:49:18 -0700

    Implment templating for base view [wip]

--- src/javascript/views/Base.js
+++ src/javascript/views/Base.js
@@ -1,12 +1,15 @@
 var assign = require('object-assign');
 
 var EventEmitter = require('../mixins/EventEmitter');
+var escapeObject = require('../utils/Escaper').escapeObject;
 
 
 function Base(options) {
   options || (options = {});
 
   this.nodeName = options.nodeName || this.nodeName || 'div';
+  this.data = options.data || {};
+  this.children = [];
 
   this.createNode();
 };
@@ -19,6 +22,20 @@ assign(Base.prototype, {
 
     if (this.node instanceof window.HTMLUnknownElement) {
       throw new Error('Invalid nodeName provided');
+    }
+
+    return this;
+  },
+
+  template: function () {
+    return [];
+  },
+
+  render: function () {
+    this.node.innerHtml = this.template.call(escapeObject(this.data));
+
+    if (typeof this.addChildren === 'function') {
+      this.children = this.addChildren();
     }
 
     return this;
