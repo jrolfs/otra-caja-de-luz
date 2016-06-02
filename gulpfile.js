5e2da008828b29a36d11612bd1169822e5b3e32a
Jamie Rolfs <jamie.rolfs@gmail.com>
2016-06-01 22:59:20 -0700

    Remove gulp-copy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const util = require('gulp-util');
-const copy = require('gulp-copy');
 const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
 const stream = require('vinyl-source-stream');
